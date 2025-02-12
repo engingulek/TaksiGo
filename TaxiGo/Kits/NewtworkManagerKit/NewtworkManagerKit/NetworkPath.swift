843e2278b03c70886bc81851732d7b7b53edc0ba
Engin Gülek <74055938+engingulek@users.noreply.github.com>
2025-02-12 16:12:24 +0300

    Task 1.0 finished

---
+++ TaxiGo/Kits/NewtworkManagerKit/NewtworkManagerKit/NetworkPath.swift
@@ -0,0 +1,50 @@
+//
+//  NetworkPath.swift
+//  NewtworkManagerKit
+//
+//  Created by Engin Gülek on 12.02.2025.
+//
+
+import Foundation
+import Alamofire
+
+public enum  NetworkPath {
+    case createConfirmCode(Parameters)
+}
+
+
+extension NetworkPath : TargetType {
+    var baseURL: String {
+        return Constants.baseUrl.rawValue
+    }
+    
+    var path: String {
+        switch self {
+        case .createConfirmCode:
+            return Constants.confirmCode.rawValue + Constants.createConfirmCode.rawValue
+        }
+    }
+    
+    var method: AlamofireMethod {
+        switch self {
+        case .createConfirmCode:
+            return .POST
+        }
+    }
+    
+    var requestType: RequestType {
+        switch self {
+        case .createConfirmCode(let parameters ):
+            return .requestParameters(parameters: parameters, encoding: JSONEncoding.init())
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        default:
+            return [:]
+        }
+    }
+    
+    
+}
